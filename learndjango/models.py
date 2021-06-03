83afd02a601b3c2e89a0982c0513359dcd19f30d
thanhluan18011997 <thanhluan18011997@gmail.com>
2021-06-03 16:39:54 +0700

    learing about django model

---
+++ learndjango/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class School(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50, null=False, unique=True)
+    years = models.PositiveIntegerField()
+    School = models.ForeignKey(School, on_delete=models.CASCADE, related_name="students")
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50, null=False, unique=True)
+    email = models.EmailField(max_length=40)
+    students = models.ManyToManyField(Student,  related_name="teachers")
+
+    def __str__(self):
+        return self.name
