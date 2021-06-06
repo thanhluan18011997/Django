88e70218bd0b634a5d43bdbee8c9bd7b8a7bc612
thanhluan18011997 <thanhluan18011997@gmail.com>
2021-06-06 10:24:17 +0700

    learn signal and middleware

--- Django_Practice/settings.py
+++ Django_Practice/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-!@3+#ubmz4b!mso$e%8sn_$cd-4!4e48t*9e5^rqw$^oqfjq8i
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -48,6 +46,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'demomiddleware.middleware.Demo1',
+
 ]
 
 ROOT_URLCONF = 'Django_Practice.urls'
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Django_Practice.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -83,7 +82,6 @@ DATABASES = {
         'PASSWORD': 'luan18011997',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -103,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -116,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- learndjango/models.py
+++ learndjango/models.py
@@ -33,3 +33,47 @@ class Teacher(models.Model):
 class People(models.Model):
     name = models.CharField(max_length=50)
     balance = models.PositiveIntegerField()
+
+
+# signal
+from django.conf import settings
+
+from django.dispatch import receiver
+from django.db.models.signals import (pre_save,
+                                      post_save,
+                                      pre_delete,
+                                      post_delete,
+                                      )
+
+User = settings.AUTH_USER_MODEL
+
+
+@receiver(post_save, sender=User)
+def user_post_save(*args, **kwargs):
+    instance = kwargs["instance"]
+    print(f"user with {instance.username} name saved!!!!!")
+
+
+@receiver(pre_save, sender=User)
+def user_pre_save(*args, **kwargs):
+    instance = kwargs["instance"]
+    print(f"user with {instance.username} name will save............")
+
+
+@receiver(pre_delete, sender=User)
+def user_pre_delete(instance, *args, **kwargs):
+    print(f"user with {instance.username} name will be delete............")
+    print(" <<<<backup data>>>>")
+
+
+@receiver(post_delete, sender=User)
+def user_post_delete(instance, *args, **kwargs):
+    print(f"user with {instance.username} name deleted!!!")
+
+
+# middleware
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    count = models.PositiveIntegerField()
