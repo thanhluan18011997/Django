b50be57e8e65d3ecb2498264ab139b53773b26a4
thanhluan18011997 <thanhluan18011997@gmail.com>
2021-06-07 07:02:15 +0700

    learing serializer and restapi

---
+++ learndjango/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import *
+
+
+class SimpleSchoolSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    address = serializers.CharField(max_length=100)
+
+    def create(self, validated_data):
+        if self.is_valid(raise_exception=True):
+            return School.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        if self.is_valid(raise_exception=True):
+            School.objects.filter(name=instance.name).update(**validated_data)
+            return School.objects.get(name=instance.name)
+
+
+class StudentModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = "__all__"
+        read_only_field = ("SChool",)
+
+    def create(self, validated_data):
+        print(validated_data)
+        if self.is_valid(raise_exception=True):
+            instance = Student.objects.create(**validated_data)
+            return instance
--- learndjango/views.py
+++ learndjango/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views import View
-
+from rest_framework.views import APIView
+from rest_framework import viewsets
 from .models import School, Student, Teacher
+from .serializers import *
 
 
 # Create your views
@@ -20,3 +23,58 @@ class test_class_base_view(View):
         student_list = bk.students.all()
         context = {"bk": bk, "student_list": student_list}
         return render(request, "classbaseview.html", context=context)
+
+
+# serializer
+
+
+class using_simple_school_serrializer(APIView):
+
+    def get(self, request, name, *args, **kwargs):
+        print(name)
+        school = School.objects.get(name=name)
+        print(school)
+        serializer = SimpleSchoolSerializer(school)
+        print(serializer.data)
+        return JsonResponse(data=serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = SimpleSchoolSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return JsonResponse(data=serializer.data)
+        return JsonResponse({"status": "error"})
+
+    def put(self, request, name, *args, **kwargs):
+        try:
+            instance = School.objects.get(name=name)
+        except Exception as e:
+            return JsonResponse({"status": "invalid"})
+        serializer = SimpleSchoolSerializer(data=request.data, instance=instance)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return JsonResponse(serializer.data)
+
+
+class using_school_model_serrializer(APIView):
+
+    def get(self, request, id, *args, **kwargs):
+        student = Student.objects.get(pk=id)
+        print(student)
+        serializer = StudentModelSerializer(student)
+        print(serializer.data)
+        return JsonResponse(data=serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = StudentModelSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return JsonResponse(data=serializer.data)
+        return JsonResponse({"status": "error"})
+
+
+# usiing viewset
+
+class using_viewset(viewsets.ModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SimpleSchoolSerializer
